9141e39f75a2cf44ff5e589a5afb0d8473ae3f43
Saksit1379 <wavetow1379@gmail.com>
2022-10-28 12:05:55 +0700

    .commit

--- Home.py
+++ Home.py
@@ -27,3 +27,23 @@ if st.button("แสดงการจินตทัศน์ข้อมูล
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
+
+    html_8="""
+<div style="background-color:#EE9513;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+pt_len=st.slider("กรุณาเลือกข้อมูล petal.lenght")
+pt_wid=st.slider("กรุณาเลือกข้อมูล petal.width")
+sp_len=st.number_input("กรุณาเลือกข้อมูล sepal.lenght")
+sp_wid=st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+if st.button("ทำนายผล"):
+    st.markdown("ใส่โมเดล")
+    st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
