5ca286970cf2450f1f8f7d76a1e5a0e89087a822
Saksit1379 <wavetow1379@gmail.com>
2022-10-28 10:19:21 +0700

    .commit

--- Home.py
+++ Home.py
@@ -22,8 +22,8 @@ st.dataframe(dx)
 dt=pd.read_csv('./data/iris.csv')
 st.dataframe(dt)
 
-dt1=dt.head(10)
-st.bar_chart(dt1)
+dz=dt.head(10)
+st.bar_chart(dz)
 st.balloons()
 
 st.sidebar.markdown("# วิเคราะห์รายบุคคล ")
