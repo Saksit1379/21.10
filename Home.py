21c8d940f8dff175c2606d9d21c057f7764081f2
Saksit1379 <wavetow1379@gmail.com>
2022-11-17 23:25:31 +0700

    .commit

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ html_8="""
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
-dt=pd.read_csv("./data/Smote.csv")
+dt=pd.read_csv("./data/noteBookE2new.csv")
 st.write(dt.head(10))
 dt1 = dt['sex'].sum()
 dt2 = dt['age'].sum()
